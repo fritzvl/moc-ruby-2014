abc53a550d1058d8b9fc8707d1bfa593ae17e29f
Bogdan Sergiienko <bogdan@masterofcode.com>
2014-12-23 18:36:46 +0200

    fixes

--- 5/examples/7.rb
+++ 5/examples/7.rb
@@ -1,45 +1,15 @@
-class PlayersFactory
+# Example of Proc objects preserving local context
 
-  def self.create_player(name, position, &block)
-
-    player_name = name
-
-    ->(own_block) do
-      yield
-      own_block.call
-      p "I'm #{player_name}, and I play as  #{position}"
-    end
-
-  end
-
+def counter
+  n = 0
+  return Proc.new { n+= 1 }
 end
 
-class Playground
+a = counter
+p a.call            # returns 1
+p a.call            # returns 2
 
+b = counter
+p b.call            # returns 1
 
-  attr_accessor :arena_name, :players
-
-  def initialize(arena_name, player_involved)
-
-    self.arena_name = arena_name
-    self.players = []
-    player_involved.each do |player|
-      players.push PlayersFactory.create_player(player[:name], player[:position]) { p "I`m glad to play on #{self.arena_name}" } if player.has_key?(:name) && player.has_key?(:position)
-    end
-  end
-
-  def call_over(actions)
-    self.players.each_with_index { |player, index| player.call(actions[index]) }
-  end
-
-end
-
-p_actions = []
-
-p_actions.push -> { p "Jumps and crys" }
-p_actions.push -> { p "Crys and jumps and crys" }
-p_actions.push -> { p "Stands as stone" }
-
-playground = Playground.new("Football arena #1", [{name: "Bill", position: "Cornerback"}, {name: "Otto", position: "Wide Receiver"}, {name: "Lenny", position: "End"}])
-
-playground.call_over(p_actions)
+p a.call            # returns 3
