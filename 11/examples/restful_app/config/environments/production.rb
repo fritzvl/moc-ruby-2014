f859a0cb71e50f5b495545beeff40c9baa45dfe1
Bogdan Sergiienko <bogdan@masterofcode.com>
2015-03-10 15:35:46 +0200

    credential update

--- 11/examples/restful_app/config/environments/production.rb
+++ 11/examples/restful_app/config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
       :address   => "smtp.mandrillapp.com",
       :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
       :enable_starttls_auto => true, # detects and uses STARTTLS
-      :user_name => "fritzvl@gmail.com",
+      :user_name => "",
       :password  => "", # SMTP password is any valid API key
       :authentication => 'login', # Mandrill supports 'plain' or 'login'
       :domain => 'yourdomain.com', # your domain to identify your server when connecting
