9d4ee78d5c1428728718d96d6fc55e14cbc76d05
Bogdan Sergiienko <bogdan@masterofcode.com>
2015-04-02 15:15:26 +0300

    variables example and YAML constants

--- 14/examples/app/helpers/articles_helper.rb
+++ 14/examples/app/helpers/articles_helper.rb
@@ -1,2 +1,7 @@
 module ArticlesHelper
+
+  def counter
+    Article.all.count
+  end
+
 end
---
+++ 14/examples/config/initializers/app_constants.rb
@@ -0,0 +1,3 @@
+# application.rb
+document   = YAML.load(File.read(File.expand_path('../../app_config.yml', __FILE__)))
+APP_CONFIG = document[Rails.env]
